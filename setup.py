dbc73eb109bfaa02dfe8b229f8cf64ff0e02626d
Kurt Rose <kurt.rose@simplelegal.com>
2018-08-23 15:49:12 -0700

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 
 __author__ = 'Kurt Rose'
-__version__ = '0.6dev'
+__version__ = '0.7dev'
 __contact__ = 'kurt@kurtrose.com'
 __url__ = 'https://github.com/kurtbrose/objex'
 __license__ = 'BSD'
