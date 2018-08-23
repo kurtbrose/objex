7eb2c0b0f507c6280f7d3c9c86435e2d985304d8
Kurt Rose <kurt.rose@simplelegal.com>
2018-08-23 15:51:09 -0700

    added creating analysis DB to test

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import os
 
-from objex import dump_graph
+from objex import dump_graph, make_analysis_db
 
 if os.path.exists('objex-test.db'):
     os.remove('objex-test.db')
 dump_graph('objex-test.db', print_info=True)
+make_analysis_db('objex-test.db', 'objex-test-analysis.db')
+
