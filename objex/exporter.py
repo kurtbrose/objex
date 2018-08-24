823b01e2059862bf3d579165346fc4e7e0f61c8c
Kurt Rose <kurt.rose@simplelegal.com>
2018-08-24 15:39:40 -0700

    another weird __module__ situation

--- objex/exporter.py
+++ objex/exporter.py
@@ -172,8 +172,10 @@ class _Writer(object):
             )
 
     def _module_name2obj_id(self, name):
-        if name is None:
-            return None  # sometimes __module__ is None
+        if not isinstance(name, str):
+            # TODO: sometimes None, sometimes random stuff (property?)
+            # is there a better heuristic for this?
+            return None
         if name not in self.modules_map:
             self.modules_map[name] = types.ModuleType(name)
         return self._ensure_db_id(self.modules_map[name])
