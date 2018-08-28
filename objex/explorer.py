595c16872ac329fc77ee9279cabaae204279492a
Kurt Rose <kurt.rose@simplelegal.com>
2018-08-28 15:48:35 -0700

    tweak to get_orphan_type_count to make it work better at console

--- objex/explorer.py
+++ objex/explorer.py
@@ -358,14 +358,15 @@ class Reader(object):
     def get_orphan_count(self):
         return self.sql_val('SELECT count(*) FROM object WHERE id NOT IN (SELECT dst FROM reference)')
 
-    def get_orphan_type_count(self):
-        return dict(self.sql(
+    def get_orphan_type_count(self, limit=20):
+        return self.sql(
             """
             SELECT name, count(object.id) FROM object JOIN pytype ON object.pytype = pytype.object
             WHERE object.id NOT IN (SELECT dst FROM reference)
                   AND NOT EXISTS (SELECT 1 FROM reference WHERE ref = '@' || CAST(object.id AS TEXT))
-            GROUP BY name
-            """))
+            GROUP BY name ORDER BY count(object.id) DESC LIMIT ?
+            """,
+            (limit,))
 
     def most_common_types(self, limit=20):
         """return the most common types, in the form [(number, type-obj-id), ...]"""
