b844a537563a42ae5612c7de1e31d3ad5b667236
Kurt Rose <kurt.rose@simplelegal.com>
2018-08-30 16:26:53 -0700

    fix CLI

--- objex/__main__.py
+++ objex/__main__.py
@@ -6,10 +6,10 @@ from . import explorer
 
 if len(sys.argv) >= 3 and sys.argv[-3] == 'make-analysis-db':
     explorer.make_analysis_db(sys.argv[-2], sys.argv[-1])
-
-try:
-    explorer.Console(explorer.Reader(sys.argv[-1])).run()
-except Exception:
-    if os.getenv('OBJEX_DEBUG', ''):
-        import traceback; traceback.print_exc()
-        import pdb; pdb.post_mortem()
+else:
+    try:
+        explorer.Console(explorer.Reader(sys.argv[-1])).run()
+    except Exception:
+        if os.getenv('OBJEX_DEBUG', ''):
+            import traceback; traceback.print_exc()
+            import pdb; pdb.post_mortem()
